3e10d5ff088ba9d03fcb29cb7cbe5bd7878ea645
Adrian Castillejos <zioyero@gmail.com>
2013-12-15 18:31:54 -0800

    Fixed bug in RKVotable caused by checking a value against NSNull instead of nil

--- Classes/Model/RKVotable.m
+++ Classes/Model/RKVotable.m
@@ -71,7 +71,7 @@
 + (NSValueTransformer *)voteStatusJSONTransformer
 {
     return [MTLValueTransformer transformerWithBlock:^(id forward) {
-        if (forward == [NSNull null])
+        if (!forward)
         {
             return @(RKVoteStatusNone);
         }
