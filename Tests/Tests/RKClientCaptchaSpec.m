c94a3e5dee3ac64a1dc76e2127614a18825ec33a
Sam Symons <sam@samsymons.com>
2014-06-23 10:55:48 -0700

    Add some tests for the CAPTCHA category.

--- Tests/Tests/RKClientCaptchaSpec.m
+++ Tests/Tests/RKClientCaptchaSpec.m
@@ -15,6 +15,10 @@ describe(@"URLForCaptchaWithIdentifier:", ^{
         NSURL *captchaURL = [client URLForCaptchaWithIdentifier:@"12345"];
         expect(captchaURL).to.equal([NSURL URLWithString:@"http://reddit.com/captcha/12345.png"]);
     });
+    
+    it(@"requires than an identifier is passed", ^{
+        expect(^{ [client URLForCaptchaWithIdentifier:nil]; }).to.raise(@"NSInternalInconsistencyException");
+    });
 });
 
 SpecEnd
