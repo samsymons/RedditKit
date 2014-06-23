7252f808129383bdb3d9cde919fb6044b47da1e3
Sam Symons <sam@samsymons.com>
2014-06-22 18:38:13 -0700

    Integrate Specta and Expecta and move the first test file over.

---
+++ Tests/Tests/RKClientSpec.m
@@ -0,0 +1,28 @@
+//
+//  RKClientSpec.m
+//  Tests
+//
+//  Created by Sam Symons on 2014-06-22.
+//
+//
+
+SpecBegin(RKClient);
+
+RKClient *client = [[RKClient alloc] init];
+client.modhash = @"12345";
+client.sessionIdentifier = @"12345";
+
+describe(@"isSignedIn", ^{
+    it(@"should return whether the user is signed in", ^{
+        expect(client.isSignedIn).to.beTruthy;
+    });
+});
+
+describe(@"signOut", ^{
+    it(@"should sign the user out", ^{
+        [client signOut];
+        expect(client.isSignedIn).to.beFalsy;
+    });
+});
+
+SpecEnd
