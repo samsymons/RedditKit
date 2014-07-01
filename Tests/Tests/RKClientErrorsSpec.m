39c4002b16bb602f0cd25476d783dc183f018044
Sam Symons <sam@samsymons.com>
2014-06-30 19:24:41 -0700

    Add stubs for the error handling tests.

---
+++ Tests/Tests/RKClientErrorsSpec.m
@@ -0,0 +1,19 @@
+//
+//  RKErrorHandlingSpec.m
+//  Tests
+//
+//  Created by Sam Symons on 2014-06-30.
+//
+//
+
+SpecBegin(RKClient_Errors);
+
+describe(@"successful responses", ^{
+    
+});
+
+describe(@"failed responses", ^{
+    
+});
+
+SpecEnd
