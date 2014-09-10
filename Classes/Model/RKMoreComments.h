522b6f751eb890383af17cd3cb8206c5a7412eef
Sam Symons <sam@samsymons.com>
2014-09-10 14:28:05 -0700

    Document the RKMoreComments object.

--- Classes/Model/RKMoreComments.h
+++ Classes/Model/RKMoreComments.h
@@ -10,10 +10,19 @@
 
 @interface RKMoreComments : RKThing
 
+/**
+ The number of comments referenced by this object.
+ */
 @property (nonatomic, assign, readonly) NSUInteger count;
 
+/**
+ The full name of this object's parent, which is an RKComment object.
+ */
 @property (nonatomic, strong, readonly) NSString *parentFullName;
 
+/**
+ The identifiers of the comments referenced by this object.
+ */
 @property (nonatomic, strong, readonly) NSArray *childIdentifiers;
 
 @end
